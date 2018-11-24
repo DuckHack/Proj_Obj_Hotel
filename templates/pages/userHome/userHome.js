1960fe9693a2514f24fdf83eb78e09aae05b16fa
Bartosz Bukowiec <bartosz.bukowiec@synerise.com>
2018-11-24 22:59:42 +0100

    get search result

--- templates/pages/userHome/userHome.js
+++ templates/pages/userHome/userHome.js
@@ -1,15 +1,50 @@
 $(document).ready(function () {
-    $('.input-daterange').datepicker({});
+    var userHome = (function () {
+        var init = function () {
+            $('.input-daterange').datepicker({
+                format: 'dd-mm-yyyy'
+            });
+            $("#range_03").ionRangeSlider({
+                type: "double",
+                grid: true,
+                min: 0,
+                max: 1000,
+                from: 0,
+                to: 1000,
+                postfix: "zł"
+            });
+        };
 
-    $("#range_03").ionRangeSlider({
-        type: "double",
-        grid: true,
-        min: 0,
-        max: 1000,
-        from: 0,
-        to: 1000,
-        postfix: "zł"
-    });
+        var getSearch = function () {
+            var searchObj = {
+                dateStart: $('#datepicker [name="start"]').val(),
+                dateEnd: $('#datepicker [name="end"]').val(),
+                priceStart: $('#range_03').val().split(';')[0],
+                priceEnd: $('#range_03').val().split(';')[1],
+                adult: $('#typeAdult').val(),
+                children: $('#typeChild').val(),
+                rating: $('#typeRating').val(),
+                typpe: $('#typeRoom').val(),
+                wifi: $("#wifi").is(':checked'),
+                conditioning: $("#conditioning").is(':checked'),
+                petFriendly: $("#petFriendly").is(':checked')
+            }
+            console.log(searchObj);
+        };
+
+        return {
+            init: init,
+            getSearch: getSearch
+        };
+    })();
+    userHome.init();
+
+
+    $('#search').click(function () {
+        userHome.getSearch();
+    })
+
+
 
 
 })
