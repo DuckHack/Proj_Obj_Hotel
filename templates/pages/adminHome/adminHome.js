fd872e2118af5fb45ae211d4b62f9c575dd2b888
Bartosz Bukowiec <bartosz.bukowiec@synerise.com>
2018-11-25 01:38:51 +0100

    admin view and notify about response

--- templates/pages/adminHome/adminHome.js
+++ templates/pages/adminHome/adminHome.js
@@ -1,22 +1,61 @@
 $(document).ready(function () {
+    var adminHome = (function () {
+        var init = function () {
+            getRooms();
+            getVisitors();
+        };
 
-    // GET REQUEST
-    $("#getAllCustomerId").click(function (event) {
-        event.preventDefault();
-        ajaxGet();
-    });
+        var addRoom = function () {
+            $.notifyDefaults({
+                type: 'success',
+                allow_dismiss: false
+            });
+            $.notify('Room added successfully!');
+            getRooms();
+        };
 
-    // DO GET
-    function ajaxGet() {
-        $.ajax({
-            type: "GET",
-            url: "http://localhost:8080/rooms/getAll",
-            success: function (result) {
-                console.log(result)
-            },
-            error: function (e) {
-                console.log("ERROR: ", e);
-            }
-        });
-    }
+        var removeRoom = function () {
+            $.notifyDefaults({
+                type: 'success',
+                allow_dismiss: false
+            });
+            $.notify('Room removed successfully!');
+            getRooms();
+        };
+
+        var getRooms = function () {
+            $.notifyDefaults({
+                type: 'success',
+                allow_dismiss: false
+            });
+            $.notify('Rooms list is up to date!');
+        };
+
+        var getVisitors = function () {
+            $.notifyDefaults({
+                type: 'success',
+                allow_dismiss: false
+            });
+            $.notify('Visitors list is up to date!');
+        };
+
+        return {
+            init: init,
+            addRoom: addRoom,
+            removeRoom: removeRoom,
+            getRooms: getRooms,
+            getVisitors: getVisitors
+        };
+    })();
+    adminHome.init();
+
+
+    $('#addRoom').click(function () {
+        adminHome.addRoom();
+    })
+
+    $('.removeRoom').click(function () {
+        adminHome.removeRoom();
+    })
+
 })
